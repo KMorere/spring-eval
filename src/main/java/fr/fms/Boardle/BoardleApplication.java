da3906ab0efbe9d62ab014b0342dcc684b7d19a6
Killian Morere <killian.morere@fms-ea.com>
2026-04-03 13:45:03 +0200

    Project Creation.

---
+++ src/main/java/fr/fms/Boardle/BoardleApplication.java
@@ -0,0 +1,13 @@
+package fr.fms.Boardle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/fms/Boardle/BoardleApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fms.Boardle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
