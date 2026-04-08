32f3a1686910b02ab21a12ad2bdf3fda261e6461
Killian Morere <killian.morere@fms-ea.com>
2026-04-08 14:39:44 +0200

    Added Task deletion.

--- src/main/java/fr/fms/Boardle/web/TaskController.java
+++ src/main/java/fr/fms/Boardle/web/TaskController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -68,6 +69,13 @@ public class TaskController {
     public String update(Task task) {
         System.out.println(task);
         taskRepository.save(task);
+        return "redirect:/index";
+    }
+
+    @DeleteMapping("/delete_task")
+    public String delete(Long id) {
+        taskRepository.deleteById(id);
+
         return "redirect:/index";
     }
 }
